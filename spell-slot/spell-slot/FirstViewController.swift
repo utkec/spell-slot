5e9121bb1cb069cb202c30b369fe3796dc2c5ae4
Casey Utke <caseyutke@icloud.com>
2019-03-30 19:44:12 -0500

    Initial Xcode project push
    Initial push of Xcode project plus some UI stuff

---
+++ spell-slot/spell-slot/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  spell-slot
+//
+//  Created by Casey Utke on 3/29/19.
+//  Copyright © 2019 Casey Utke. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
