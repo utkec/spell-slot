af7b928a75971ab3aa8d539326fe393d6cfef76a
Casey Utke <caseyutke@icloud.com>
2019-07-07 19:42:26 -0500

    Added data shown on Spell Detail viewController

--- spell slot/DetailViewController.swift
+++ spell slot/DetailViewController.swift
@@ -10,21 +10,53 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    
     @IBOutlet weak var spellName: UILabel!
     @IBOutlet weak var spellLevel: UILabel!
     @IBOutlet weak var spellSchool: UILabel!
     @IBOutlet weak var spellCastTime: UILabel!
     @IBOutlet weak var spellDuration: UILabel!
     @IBOutlet weak var spellRange: UILabel!
-    @IBOutlet weak var spellComp: UILabel!
+    @IBOutlet weak var spellComponents: UILabel!
     @IBOutlet weak var spellClass: UILabel!
-    @IBOutlet weak var spellDesc: UILabel!
+    @IBOutlet weak var spellDescription: UILabel!
+    
+    
+
+    var sName: String?
+    var sLevel: Int!
+    var sSchool: String?
+    var sCastTime: String?
+    var sDuration: String?
+    var sRange: String?
+    var sComponents: String?
+    var sClass: [String]?
+    var sDescription: String?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.spellName?.text = "test"
+        
+        spellName.text = sName
+        
+        
+        if (sLevel == 0) {
+            spellLevel.text = "Cantrip"
+        } else {
+            spellLevel.text = "Level " + "\(sLevel!)"
+        }
+        
+        spellSchool.text = "School: " + sSchool!.capitalized
+        spellCastTime.text = "Cast Time: " + sCastTime!
+        spellDuration.text = "Duration: " + sDuration!
+        spellRange.text = "Range: " + sRange!
+        spellComponents.text = sComponents
+        
+        for (index, element) in sClass!.enumerated() {
+            sClass![index] = element.capitalized
+        }
+        spellClass.text = "Class: " + (sClass?.joined(separator: ", "))!
+//        spellDescription.text = sDescription
+        
         
     }
     
--- spell slot/ViewController.swift
+++ spell slot/ViewController.swift
@@ -12,10 +12,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     @IBOutlet weak var spellTable: UITableView!
     
-    
     // Create empty spellList qrray
     var spellList = [Spell]()
-    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // Make enough cells for the number of spell structs in spellList array
@@ -28,6 +26,33 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.textLabel?.text = spellList[indexPath.row].name
         cell.detailTextLabel?.text = "\(spellList[indexPath.row].level)"
         return cell
+    }
+    
+    
+    
+    var rowNum = 0;
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        rowNum = indexPath.row
+        // Segue to the second view controller
+        self.performSegue(withIdentifier: "spellInfo", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // get a reference to the second view controller
+        let secondViewController = segue.destination as! DetailViewController
+        
+        // set a variable in the second view controller with the data to pass
+        secondViewController.sName = spellList[rowNum].name
+        secondViewController.sLevel = spellList[rowNum].level
+        secondViewController.sSchool = spellList[rowNum].school
+        secondViewController.sCastTime = spellList[rowNum].casting_time
+        secondViewController.sDuration = spellList[rowNum].duration
+        secondViewController.sRange = spellList[rowNum].range
+        secondViewController.sComponents = spellList[rowNum].components.raw
+        secondViewController.sClass = spellList[rowNum].classes
+        secondViewController.sDescription = spellList[rowNum].description
     }
     
     
@@ -57,4 +82,5 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             }
             }.resume()
     }
+    
 }
