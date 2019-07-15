4eb70238ad8e1d35160d351adca7626017016e1d
Casey Utke <caseyutke@icloud.com>
2019-07-15 16:39:57 -0500

    Fixed Scrolling Issue for spell description
    Made description bottom constraint = to 0

---
+++ spell slotTests/spell_slotTests.swift
@@ -0,0 +1,32 @@
+//
+//  spell_slotTests.swift
+//  spell slotTests
+//
+//  Created by Casey Utke on 7/10/19.
+//  Copyright © 2019 Casey Utke. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+@testable import spell_slot
+
+class spell_slotTests: XCTestCase {
+    
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        
+        
+        
+    }
+
+
+}
